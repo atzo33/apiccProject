ac96dde6867f3cc28a9a87040705f857d03cccae
Atzo <cofa.pavlovic3@gmail.com>
2023-05-07 18:38:14 +0200

    Initial push

---
+++ src/main/java/apicc/ApiccApplication.java
@@ -0,0 +1,13 @@
+package apicc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apicc/ApiccApplicationTests.java
@@ -0,0 +1,13 @@
+package apicc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
